910bb75f979d9a1923d14bd389e8196dd8497132
Angie-cs <gelisscs@gmail.com>
2026-01-24 17:17:59 -0600

    Implement SJF and RR schedulers

--- src/schdl/rr.c
+++ src/schdl/rr.c
@@ -4,12 +4,58 @@
 /* ============================================================
  * Student implementation area
  * ============================================================ */
+
 void rr_schedule(Process p[], int n, int quantum)
 {
-    (void)p;
-    (void)n;
-    (void)quantum;
-    /* TODO: Implement RR scheduling algorithm here */
+    int time = 0;
+    int completed = 0;
+
+    int remaining[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        remaining[i] = p[i].burst_time;
+        p[i].waiting_time = 0;
+        p[i].turnaround_time = 0;
+        p[i].completed = 0;
+    }
+
+    while (completed < n)
+    {
+        int executed = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            if (remaining[i] > 0 && p[i].arrival_time <= time)
+            {
+                executed = 1;
+
+                if (remaining[i] > quantum)
+                {
+                    time += quantum;
+                    remaining[i] -= quantum;
+                }
+                else
+                {
+                    time += remaining[i];
+                    remaining[i] = 0;
+
+                    p[i].turnaround_time =
+                        time - p[i].arrival_time;
+
+                    p[i].waiting_time =
+                        p[i].turnaround_time - p[i].burst_time;
+
+                    p[i].completed = 1;
+                    completed++;
+                }
+            }
+        }
+
+        /* Si nadie pudo ejecutarse, avanzar tiempo */
+        if (!executed)
+            time++;
+    }
 }
 
 /* ============================================================
